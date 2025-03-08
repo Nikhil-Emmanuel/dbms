61028f5391c7431df7382f4667de1fe0f8d124e3
T Nikhil Emmanuel <99235919+Nikhil-Emmanuel@users.noreply.github.com>
2025-03-08 21:58:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ const MONGO_URI = process.env.MONGO_URI;  // Get MongoDB URI from .env
 // Middleware
 app.use(cors({
     origin: "http://54.91.93.132",  // Allow requests from this IP
-    methods: ["GET", "POST"],      // Allow only GET and POST methods (you can add others if needed)
-    allowedHeaders: ["Content-Type", "Authorization"]  // Allow only specific headers
+    // Allow only specific headers
 }));
- Allow requests from the front-end port
-}));
+
 app.use(express.json());
 
 // Connect to MongoDB Atlas
